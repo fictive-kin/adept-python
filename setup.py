86f318b9da1e1d534cf30c0b403ce91651109cc3
Phil Howell <phil@quae.co.uk>
2014-02-04 18:07:45 +0000

    Ignore packaging stuff

--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@
 import os
 from setuptools import setup
 
-
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 with open('requirements.txt') as f:
-    required = f.read().splitlines()
+    requires = f.read().splitlines()
 
 setup(
-    name="adept",
+    name="Adept",
     version="0.0.1",
     author="Fictive Kin",
     maintainer="Phil Howell",
     author_email="phil@fictivekin.com",
     description="Simple binding for Adept",
-    license="BSD",
-    install_requires=required,
+    license="MIT",
+    install_requires=requires,
     keywords=['adept', 'image', 'resize', 'processing'],
     url="https://github.com/fictivekin/adept-python",
     download_url="https://github.com/fictivekin/adept-python/archive/0.0.1.tar.gz",
     packages=['adept'],
+    package_data={'': ['LICENSE', 'README.md', 'requirements.txt']},
     long_description=read('README.md'),
     classifiers=[
         "Environment :: Web Environment",
